e6dc84f909391f09899a58a9ec937e2d60193e41
Henrique Ataide <henriqueataide.dev@gmail.com>
2024-06-28 19:33:28 -0300

    tipagem metadata

--- solo-website/src/app/layout.tsx
+++ solo-website/src/app/layout.tsx
@@ -1,47 +1,49 @@
 import { ReactNode } from 'react';
 import Head from 'next/head';
 import { Toaster } from "@/components/ui/toaster";
-import { Inter } from 'next/font/google';
+import {Metadata} from 'next'
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+type RootLayoutProps = {
+  children: ReactNode;
+};
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Solo',
-  description: 'Solo Solutions licensed',
+  description: 'Soluções inteligentes para empresas inteligentes',
   icons: {
     icon: ['/favicon/favicon.ico'],
     apple: ['/apple-touch-icon.png?v=4'],
     shortcut: ['/apple-touch-icon.png']
   }
-};
+}
 
-type RootLayoutProps = {
-  children: ReactNode;
-  metadata?: {
-    title: string;
-    description: string;
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  const defaultMetadata = {
+    title: 'Solo Solutions',
+    description: 'Soluções inteligentes para empresas inteligentes'
+    
   };
-};
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children, metadata = { title: '', description: '' } }) => {
   return (
-    <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <title>{metadata.title}</title>
-      </Head>
-      <body>
-        <main>{children}</main>
-        <Toaster />
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content={defaultMetadata.description} />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=4" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <title>{defaultMetadata.title}</title>
+        </Head>
+        <body>
+          <main>{children}</main>
+          <Toaster />
+        </body>
+      </html>
+    </>
   );
 };
 
