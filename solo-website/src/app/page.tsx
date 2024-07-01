68a4778385a769ed3798ef95f7b3c5509639020c
Henrique Ataide <henriqueataide.dev@gmail.com>
2024-07-01 18:08:23 -0300

    caminho imagem corrigido

--- solo-website/src/app/page.tsx
+++ solo-website/src/app/page.tsx
@@ -262,7 +262,7 @@ export default function Component() {
                   </div>
                   <div className="flex items-center justify-center">
                     <img
-                      src="./ERPS2_pagina.svg"
+                      src="/erps_pagina.svg"
                       width="550"
                       height="310"
                       alt="ERPS"
