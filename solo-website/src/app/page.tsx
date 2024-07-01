e5b5652c5c04f78c4549a50b0276ee1ea54ddb29
Henrique Ataide <henriqueataide.dev@gmail.com>
2024-07-01 19:42:22 -0300

    ajustes finais

--- solo-website/src/app/page.tsx
+++ solo-website/src/app/page.tsx
@@ -233,46 +233,6 @@ export default function Component() {
             </section>
 
             <section ref={(el) => {
-          if (el) sectionRefs.current[3] = el as HTMLDivElement;
-        }} className=" text-white py-24 md:py-36 lg:py-40">
-              <div className="container px-4 md:px-6">
-                <div className="grid gap-6 lg:grid-cols-2 lg:gap-12">
-                  <div className="space-y-4">
-                    <div className="inline-block rounded-lg bg-azulsolo text-white px-3 py-1 text-sm">Integrações</div>
-                    <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                      Conecte seus sistemas com facilidade
-                    </h2>
-                    <p className="max-w-[600px] text-gray-300 md:text-xl">
-                      Integre seus sistemas e aplicativos corporativos com a Solo e automatize seus fluxos de trabalho.
-                    </p>
-                    <ul className="grid gap-4">
-                      <li className="flex items-center gap-2">
-                        <CheckIcon className="h-5 w-5 text-green-500" />
-                        <span>CRMs</span>
-                      </li>
-                      <li className="flex items-center gap-2">
-                        <CheckIcon className="h-5 w-5 text-green-500" />
-                        <span>ERPs</span>
-                      </li>
-                      <li className="flex items-center gap-2">
-                        <CheckIcon className="h-5 w-5 text-green-500" />
-                        <span>E-commerce</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <img
-                      src="/erps2_pagina.svg"
-                      width="550"
-                      height="310"
-                      alt="ERPS"
-                      className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
-                    />
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section ref={(el) => {
           if (el) sectionRefs.current[4] = el as HTMLDivElement;
         }} className="py-20 md:py-36 lg:py-40  w-full flex items-center justify-center">
               <div className="container px-4 md:px-6 ">
